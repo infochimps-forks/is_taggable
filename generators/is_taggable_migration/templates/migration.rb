ad007ff38443de42384e66a0c6ef6dd55ece411a
Philip (flip) Kromer <flip@infochimps.org>
2009-04-25 01:34:49 -0500

    unique indexes; length constraints

--- generators/is_taggable_migration/templates/migration.rb
+++ generators/is_taggable_migration/templates/migration.rb
@@ -1,22 +1,23 @@
 class IsTaggableMigration < ActiveRecord::Migration
+  MAX_STR_LEN_TAG_NAME   = 100
+  MAX_STR_LEN_MODEL_NAME = 20
   def self.up
     create_table :tags do |t|
-      t.string :name, :default => ''
-      t.string :kind, :default => '' 
+      t.string :name, :default => '', :limit => MAX_STR_LEN_TAG_NAME
+      t.string :kind, :default => '', :limit => MAX_STR_LEN_MODEL_NAME
     end
 
     create_table :taggings do |t|
       t.integer :tag_id
-
-      t.string  :taggable_type, :default => ''
+      t.string  :taggable_type, :default => '', :limit => MAX_STR_LEN_MODEL_NAME
       t.integer :taggable_id
     end
-    
-    add_index :tags,     [:name, :kind]
-    add_index :taggings, :tag_id
-    add_index :taggings, [:taggable_id, :taggable_type]
+
+    add_index :tags,     [:name, :kind], :unique => true
+    add_index :taggings, [:tag_id]
+    add_index :taggings, [:taggable_id, :taggable_type, :tag_id], :unique => true
   end
-  
+
   def self.down
     drop_table :taggings
     drop_table :tags
