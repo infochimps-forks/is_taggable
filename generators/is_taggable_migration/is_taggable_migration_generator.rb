7d04fcb33118ec6e8c80546765b03333e3e6986d
Philip (flip) Kromer <flip@infochimps.org>
2009-05-10 06:11:06 -0500

    Backported changes to migration

--- generators/is_taggable_migration/is_taggable_migration_generator.rb
+++ generators/is_taggable_migration/is_taggable_migration_generator.rb
@@ -1,7 +1,14 @@
-class IsTaggableMigrationGenerator < Rails::Generator::Base 
-  def manifest 
-    record do |m| 
+class IsTaggableMigrationGenerator < Rails::Generator::Base
+  def manifest
+    record do |m|
       m.migration_template 'migration.rb', 'db/migrate', :migration_file_name => 'is_taggable_migration'
-    end 
+      m.directory 'config/initializers/'
+      m.file      'config/initializers/is_taggable_constraints.rb', 'config/initializers/is_taggable_constraints.rb'
+    end
   end
+
+  def banner
+    "\nUsage: script/generate is_taggable_migration.\n\n"
+  end
+
 end
--- generators/is_taggable_migration/templates/migration.rb
+++ generators/is_taggable_migration/templates/migration.rb
@@ -1,25 +1,24 @@
 class IsTaggableMigration < ActiveRecord::Migration
-  MAX_STR_LEN_TAG_NAME   = 100
-  MAX_STR_LEN_MODEL_NAME = 20
   def self.up
     create_table :tags do |t|
-      t.string :name, :default => '', :limit => MAX_STR_LEN_TAG_NAME
-      t.string :kind, :default => '', :limit => MAX_STR_LEN_MODEL_NAME
+      t.string   :name,           :limit => MAX_TAG_NAME_LEN,   :default => ''
+      t.string   :kind,           :limit => MAX_MODEL_NAME_LEN, :default => ''
     end
 
     create_table :taggings do |t|
-      t.integer :tag_id
-      t.string  :taggable_type, :default => '', :limit => MAX_STR_LEN_MODEL_NAME
-      t.integer :taggable_id
+      t.integer  :tag_id
+      t.integer  :taggable_id
+      t.string   :taggable_type,  :limit => MAX_MODEL_NAME_LEN, :default => ''
     end
 
-    add_index :tags,     [:name, :kind], :unique => true
-    add_index :taggings, [:tag_id]
-    add_index :taggings, [:taggable_id, :taggable_type, :tag_id], :unique => true
+    add_index    :tags,        [:name, :kind], :unique => true
+    add_index    :tags,        [:kind]   # remove this if you'll only have one or two kinds of tags
+    add_index    :taggings,    [:tag_id]
+    add_index    :taggings,    [:taggable_id, :taggable_type, :tag_id], :unique => true
   end
 
   def self.down
-    drop_table :taggings
-    drop_table :tags
+    drop_table   :taggings
+    drop_table   :tags
   end
 end
